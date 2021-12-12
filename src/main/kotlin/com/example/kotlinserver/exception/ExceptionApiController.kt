c2cebc519be338dae9895ec0a2592e21c5080017
kwonc <jabykw@gmail.com>
2021-12-12 18:25:58 +0900

    exception handler detail

---
+++ src/main/kotlin/com/example/kotlinserver/exception/ExceptionApiController.kt
@@ -0,0 +1,61 @@
+package com.example.kotlinserver.exception
+
+import com.example.kotlinserver.model.Error
+import com.example.kotlinserver.model.ErrorResponse
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.*
+import java.time.LocalDateTime
+import javax.servlet.http.HttpServletRequest
+import javax.validation.ConstraintViolationException
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
+
+@RestController
+@RequestMapping("/api/exception")
+@Validated
+class ExceptionApiController {
+
+    @GetMapping("")
+    fun get(
+        @NotBlank
+        @Size(min = 2, max = 6)
+        @RequestParam name:String,
+
+        @Min(10)
+        @RequestParam age:Int
+    ): String{
+        println(name)
+        println(age)
+
+        return name + " " + age
+    }
+
+    @ExceptionHandler(value = [ConstraintViolationException::class])
+    fun constraintViolationException(e: ConstraintViolationException, request: HttpServletRequest): ResponseEntity<ErrorResponse>{
+        val errors = mutableListOf<Error>()
+
+        e.constraintViolations.forEach{
+            val error = Error().apply {
+                this.field = it.propertyPath.last().name
+                this.message = it.message
+                this.value = it.invalidValue
+            }
+            errors.add(error)
+        }
+
+        val errorResponse = ErrorResponse().apply{
+            this.result_code = "FAIL"
+            this.http_status = HttpStatus.BAD_REQUEST.value().toString()
+            this.http_method = request.method
+            this.message = "error"
+            this.path = request.requestURI.toString()
+            this.timestamp = LocalDateTime.now()
+            this.errors = errors
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse)
+    }
+}
